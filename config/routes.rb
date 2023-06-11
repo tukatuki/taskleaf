51290cfef825de3a4ca13c22e5aac926cbd72160
tukatuki <h.naruke0430@gmail.com>
2023-06-11 14:29:26 +0900

    genba_rails_p163

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get '/login', to: 'sessions#new'
+  
+  namespace :admin do
+    resources :users
+  end
   root to: 'tasks#index'
   resources :tasks
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_05_205120) do
+ActiveRecord::Schema.define(version: 2023_06_10_024845) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
