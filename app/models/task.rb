865b044d185ffc0027b00c7b69034a889c5f7083
tukatuki <h.naruke0430@gmail.com>
2023-06-15 22:01:39 +0900

    genbaRails_224

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,4 @@
 class Task < ApplicationRecord
-  before_validation :set_nameless_name
 
   validates :name, presence: true
   validates :name, length: { maximum: 30 }
@@ -10,10 +9,6 @@ class Task < ApplicationRecord
   scope :recent, -> { order(created_at: :desc) }
 
   private
-
-  def set_nameless_name
-    self.name = '名前なし' if name.blank?
-  end
 
   def validate_name_not_including_comma
     errors.add(:name, 'にコンマを含めることはできません') if name&.include?(',')
