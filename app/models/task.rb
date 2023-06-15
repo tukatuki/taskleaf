becdb01afbdd23c4a5b7282628f83d556d22d8b4
tukatuki <h.naruke0430@gmail.com>
2023-06-15 20:34:12 +0900

    gengaRails_219

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,12 +3,15 @@ class Task < ApplicationRecord
 
   validates :name, presence: true
   validates :name, length: { maximum: 30 }
-
   validate :validate_name_not_including_comma
+
+  belongs_to :user
+
+  scope :recent, -> { order(created_at: :desc) }
 
   private
 
-  def set_nameless_namej
+  def set_nameless_name
     self.name = '名前なし' if name.blank?
   end
 
